55959e10640ae005045a01220f3adf13db1fb014
Alex Wood <github@alex-wood.org.uk>
2016-09-03 02:25:06 +0100

    Added configurable app, config and comics directories

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-
-cd /home/comicstreamer/ComicStreamer
+cd "${APP}/ComicStreamer"
 
 # Update repository
 echo -e "Update comicstreamer..."
 git pull 
 
-OPTIONS="--nobrowser -p ${PORT}"
+OPTIONS="--nobrowser -p ${PORT} --user-dir ${CONFIG} ${DATA}"
 # Run comicstreamer
 if [ ${WEBROOT} ]; then 
 	OPTIONS="$OPTIONS --webroot ${WEBROOT}"
 fi
 
 echo -e "Launch comicstreamer with options <$OPTIONS>"
-./comicstreamer $OPTIONS "/comics"
+./comicstreamer $OPTIONS
